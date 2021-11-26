7544dee38ed4440f7f969e79ae5288699c870f07
Julian Grisales <jgrisales@gmail.com>
2021-11-26 23:19:05 +0100

    Search Page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+
+const SearchFilters = () => {
+    return (
+        <div>
+            Seearch Filters
+        </div>
+    )
+}
+
+export default SearchFilters
